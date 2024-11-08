70e7209099df3d5096c3f109865334f3e73d2cf3
Dawid Roszman <dawidroszman@protonmail.com>
2024-11-08 09:20:22 +0100

    working on client message separating

--- lab04/client.cpp
+++ lab04/client.cpp
@@ -1,16 +1,31 @@
 #include <iostream>
 #include <sys/fcntl.h>
 #include <unistd.h>
+
 int main()
 {
-  char userInput[80];
+  char userInput[80], path[80], charid[80];
+  int fd1, id, counter = 0;
+  bool seenSeparator = false;
   std::cout << "Enter query (format: <id>:<path>)" << std::endl;
   std::cin >> userInput;
-  int fd1;
-  const char *serverFifo = "serverFifo";
-  fd1 = open(serverFifo, O_WRONLY);
-  write(fd1, userInput, sizeof(userInput));
-  close(fd1);
+  for(int i = 0; i < sizeof(userInput); i++){
+    if (userInput[i] == ':'){
+      seenSeparator = true;
+      continue;
+    }
+    if(!seenSeparator){
+      counter++;
+      charid[i] = userInput[i];
+    }else {
+      path[i-counter] = userInput[i];
+    }
+  }
+  std::cout << path << ' ' << charid << std::endl;
+  /*const char *serverFifo = "serverFifo";*/
+  /*fd1 = open(serverFifo, O_WRONLY);*/
+  /*write(fd1, userInput, sizeof(userInput));*/
+  /*close(fd1);*/
 
   
   return 0;
